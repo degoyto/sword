79e41b681ae6a0b3e59a16a9440d0d37e9016f85
visitante <visitante@ffb.local>
2020-01-31 23:13:05 -0300

    segundo

---
+++ Assets/scripts/ItemBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBehaviour : MonoBehaviour
+{
+
+    public int id;
+    public string nome;
+    
+    
+}
